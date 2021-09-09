9796e363ef841420e2334ee8d5092f20df42be60
Cara Noyes <cnnnoyes@outlook.com>
2021-09-08 19:03:57 -0500

    completed

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,23 +3,38 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
+//    @GetMapping
+//    public void displayAllEvents(Model model){
+//        HashMap<String, String> events = new HashMap<>();
+//            events.put("StrangeLoop", "coding conference in September");
+//            events.put("Code With Pride", "for LGBTQ+ and allies");
+//            events.put("Unnamed Conference", "I don't know");
+////        model.addAttribute("events", events);
+////        return "events/index";
+//    }
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+        Map<String, String> eventsMap = new HashMap<>();
+        eventsMap.put("StrangeLoop", "coding conference in September");
+        eventsMap.put("Code With Pride", "focused on LGBTQ+ and allies");
+        eventsMap.put("Unnamed Conference", "I don't know");
+        model.addAttribute("events", eventsMap);
         return "events/index";
     }
 
@@ -27,6 +42,13 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+            events.add(eventName);
+            return "redirect:"; //this will redirect to the root (/events)
     }
 
 }
